4032eaa4e5cdde416526a9a73d924c36c327dded
Uditha Karunaratne <uditha.karunaratne@infor.com>
2020-08-03 23:49:27 +1000

    Decision tree classifier

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -7,25 +7,30 @@
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
+
+from sklearn.metrics import accuracy_score
+
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+from sklearn import tree
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
-
 #########################################################
 ### your code goes here ###
 
+print len(features_train[0])
 
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+clf.fit(features_train, labels_train)
+predict = clf.predict(features_test)
+
+accuracy = accuracy_score(predict, labels_test)
+print "Accuracy with min Sample Rate {} is : {}".format(40, accuracy)
 #########################################################
-
-
